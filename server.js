9076bff06573868a6a3299455a45cfbbf52ba2fd
gitsanghyun <gks7654@gmail.com>
2024-09-04 16:29:48 +0900

    server_change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("port", process.env.PORT || 3000);
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(app.get("port"), () => {
